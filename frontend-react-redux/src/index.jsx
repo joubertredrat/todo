d6a201f48c93615165babcea17442dd2c05e90f1
Joubert Guimarães de Assis <me+github@redrat.com.br>
2020-04-04 16:27:25 -0300

    ex 088 - add middleware redux multi

--- frontend-react-redux/src/index.jsx
+++ frontend-react-redux/src/index.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { applyMiddleware, createStore } from 'redux'
 import { Provider } from 'react-redux'
+
 import promisse from 'redux-promise'
+import multi from 'redux-multi'
 
 import App from './main/app'
 import reducers from './main/reducers'
@@ -10,7 +12,7 @@ import reducers from './main/reducers'
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
     && window.__REDUX_DEVTOOLS_EXTENSION__()
 
-const store = applyMiddleware(promisse)(createStore)(reducers, devTools)
+const store = applyMiddleware(promisse, multi)(createStore)(reducers, devTools)
 
 ReactDOM.render(
   <Provider store={store}>
