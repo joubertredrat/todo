74990fea3b02bae5479743db0ce640a4a9c72ae9
Joubert Guimarães de Assis <me+github@redrat.com.br>
2020-04-04 15:49:30 -0300

    ex 083 - create action creator for change description

--- frontend-react-redux/src/todo/todoForm.jsx
+++ frontend-react-redux/src/todo/todoForm.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
+import { changeDescription } from './todoActions'
 
 const TodoForm = props => {
   const keyHandler = (e) => {
@@ -21,7 +23,7 @@ const TodoForm = props => {
           className="form-control"
           placeholder="Adicione uma tarefa"
           value={props.description}
-          onChange={props.handleChange}
+          onChange={props.changeDescription}
           onKeyUp={keyHandler}
         />
       </Grid>
@@ -51,4 +53,7 @@ const mapStateToProps = state => ({
   description: state.todo.description
 })
 
-export default connect(mapStateToProps)(TodoForm)
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ changeDescription }, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
---
+++ frontend-react-redux/src/todo/todoReducer.js
@@ -0,0 +1,29 @@
+const INITIAL_STATE = {
+  description: 'Anything',
+  list: [{
+    _id: 1,
+    description: 'Todo 01',
+    done: true
+  }, {
+    _id: 2,
+    description: 'Todo 02',
+    done: false
+  }, {
+    _id: 3,
+    description: 'Todo 03',
+    done: false
+  }, {
+    _id: 4,
+    description: 'Todo 04',
+    done: false
+  }]
+}
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'DESCRIPTION_CHANGED':
+      return { ...state, description: action.payload }
+    default:
+      return state
+  }
+}
