ac93f4e44aa9bafee4c7a7398634e652dee85351
Joubert Guimarães de Assis <me+github@redrat.com.br>
2020-04-04 16:12:27 -0300

    ex 086 - add apply middleware redux promisse into app

--- frontend-react-redux/src/todo/todoReducer.js
+++ frontend-react-redux/src/todo/todoReducer.js
@@ -1,30 +1,14 @@
 const INITIAL_STATE = {
-  description: 'Anything',
-  list: [{
-    _id: 1,
-    description: 'Todo 01',
-    done: true
-  }, {
-    _id: 2,
-    description: 'Todo 02',
-    done: false
-  }, {
-    _id: 3,
-    description: 'Todo 03',
-    done: false
-  }, {
-    _id: 4,
-    description: 'Todo 04',
-    done: false
-  }]
+  description: '',
+  list: []
 }
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case 'DESCRIPTION_CHANGED':
       return { ...state, description: action.payload }
-      case 'TODO_SEARCHED':
-        return { ...state, list: action.payload.data }
+    case 'TODO_SEARCHED':
+      return { ...state, list: action.payload.data }
     default:
       return state
   }
