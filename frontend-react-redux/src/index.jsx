9a0205a3664aa27420c11f26ae15442b9188db9a
Joubert Guimarães de Assis <me+github@redrat.com.br>
2020-04-04 16:49:28 -0300

    ex 089 - add and apply middleware redux thunk

--- frontend-react-redux/src/index.jsx
+++ frontend-react-redux/src/index.jsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux'
 
 import promisse from 'redux-promise'
 import multi from 'redux-multi'
+import thunk from 'redux-thunk'
 
 import App from './main/app'
 import reducers from './main/reducers'
@@ -12,7 +13,7 @@ import reducers from './main/reducers'
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
     && window.__REDUX_DEVTOOLS_EXTENSION__()
 
-const store = applyMiddleware(promisse, multi)(createStore)(reducers, devTools)
+const store = applyMiddleware(promisse, multi, thunk)(createStore)(reducers, devTools)
 
 ReactDOM.render(
   <Provider store={store}>
